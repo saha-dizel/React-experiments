9c1301ab36866d4444510b7c418f2855d7f50e23
Alex K <saha.dizel@gmail.com>
2021-03-26 00:19:56 +0300

    React components part2 + redo of App to class

--- reactApp/testing_proc1/src/App.js
+++ reactApp/testing_proc1/src/App.js
@@ -1,21 +1,53 @@
 //import logo from './logo.svg';
+import { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
-import Menu from './components/MenuComponent'
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">
-            Ristorante Con Fusion
-          </NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">
+              Ristorante Con Fusion
+            </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={ this.state.dishes }/>
+      </div>
+    );
+  }
 }
+
+// function App(props) {
+//   // eslint-disable-next-line
+//   this.state = {
+//     dishes: DISHES
+//   };
+
+//   return (
+//     <div>
+//       <Navbar dark color="primary">
+//         <div className="container">
+//           <NavbarBrand href="/">
+//             Ristorante Con Fusion
+//           </NavbarBrand>
+//         </div>
+//       </Navbar>
+//       <Menu dishes={ this.state.dishes }/>
+//     </div>
+//   );
+// }
 
 export default App;
